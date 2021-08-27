d6eeca48c09b687d0fcd121bd6bcdce25c7481a5
Ulyana <83964382+Ulyana999@users.noreply.github.com>
2021-08-27 22:05:38 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,26 +1,3 @@
-// let i = 0;
-// let a = 1;
-// let sum = 0;
-// let q = 0;
-// let sum2 = 0;
-
-// while (i < 10) {
-//     i++
-//     let a = Math.pow(i, 2)
-//     sum += a
-//     while (q < 10) {
-//         q++
-//         sum2 += q
-//         p = Math.pow(sum2, 2)
-//     }
-// }
-// let differ = p - sum;
-
-// console.log(differ)
-
-
-
-
 function task_6 (n) {
     let i = 0;
     let a = 1;
@@ -39,8 +16,9 @@ function task_6 (n) {
         }
     }
     let differ = p - sum;
+
+    return(differ)
 }
 let result = task_6(10);
 console.log(result);
-
 
