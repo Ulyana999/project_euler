e598e6b51a6585e3f2f3d210b5e12eae44139199
Ulyana <83964382+Ulyana999@users.noreply.github.com>
2021-10-06 13:07:55 +0300

    Create task7v.2.js

---
+++ task7v.2.js
@@ -0,0 +1,16 @@
+function getPrimes(num) {
+    const seive = [];
+    const primes = [];
+
+    for (let i = 2; i <= num; i++) {
+        if (!seive[i]) {
+            primes.push(i);
+            for (let j = i * 2; j <= num; j += i)
+                seive[j] = true;
+        }
+    }
+
+
+    return primes[primes.length-1];
+}
+console.log(getPrimes(10001));
